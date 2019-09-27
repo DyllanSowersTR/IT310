4e7ac55394e0652c55aa4051b642e1c7f85a752e
dsowers <dyllan.sowers@gmail.com>
2019-09-26 23:35:33 -0500

    added functions, created structure for comparing sort timings

--- Project 2/Part1.py
+++ Project 2/Part1.py
@@ -1,8 +1,6 @@
 ## referenced https://runestone.academy/runestone/books/published/pythonds/Introduction/ObjectOrientedProgramminginPythonDefiningClasses.html 
 ## on 9/25/19
 
-## Questions: Do the integers and fraction objects need to be sorted in the same list?
-## May need to overload the operators to compare integers and objects, not sure if this would be in Fractions or Sort
 
 import time
 import random
@@ -11,12 +9,11 @@ class Sort(object):
     numbers = []
     elapsedTime = 0
 
-    #optionally overload with an initiating list
     def __init__(self, numList):
       self.numbers = numList
       self.numbersSize = len(self.numbers)
 
-    def SelectionSort(self):
+    def SelectionSort(self): # confirmed to work
        i = 0
        j= 0
        indexSmallest = 0
@@ -36,23 +33,23 @@ class Sort(object):
        self.updateTime(startTime, endTime)
 
 
-    def InsertionSort(self):
+    def InsertionSort(self): # confirmed to work
         i = 0
         j = 0
         temp = 0
         startTime = time.time()
         for i in range(1, self.numbersSize):
             j = i
-            while j > 0 and numbers[j] < self.numbers[j - 1]:
-                temp = numbers[j]
+            while j > 0 and self.numbers[j] < self.numbers[j - 1]:
+                temp = self.numbers[j]
                 self.numbers[j] = self.numbers[j - 1]
                 self.numbers[j - 1] = temp 
                 j -= 1
         endTime = time.time()
         self.updateTime(startTime, endTime)
  
-    def BubbleSort(self):
-        startTime = time.time()
+    def BubbleSort(self): # works, sorts in descending order though
+        startTime = time.time() 
 
         for i in range(0, self.numbersSize - 1):
             for j in range(0, self.numbersSize - i - 1):
@@ -77,7 +74,8 @@ class Fraction(object):
         self.denominator = bottom
 
     def __str__(self):
-       return str(self.numerator) + "/" + str(self.denominator)
+      # return str(self.numerator) + "/" + str(self.denominator)
+       return str(float(self.numerator/self.denominator))
 
     def asDecimal(self):
         return float(self.numerator/self.denominator)
@@ -107,22 +105,38 @@ class Fraction(object):
 
 
 def GenerateIntegerList(listSize):
-    return [random.randint(0,100) for i in range(random.randint(10,listSize))] 
+    return [random.randint(0,100) for i in range(listSize)] 
    
 def GenerateFractionList(listSize):
-    return [Fraction(random.randint(1,100), random.randint(1,100)) for i in range(random.randint(10, listSize))]
+    return [Fraction(random.randint(1,100), random.randint(1,100)) for i in range(listSize)]
+
+def getTimings(list):
+    sort = Sort(list)
+
+    sort.SelectionSort()
+    timings = [sort.elapsedTime]
+    sort.elapsedTime = 0
+
+    sort.numbers = list
+    sort.InsertionSort()
+    timings.append(sort.elapsedTime)
+    sort.elapsedTime = 0
+
+    sort.numbers = list
+    sort.BubbleSort()
+    timings.append(sort.elapsedTime)
+    sort.elapsedTime = 0
+    return timings
+
+   
 
 def main():
-    list = GenerateList() 
-    sort = Sort(list)
-    ## Printing the list itself will print memory locations for the objects for some reason
-    print("Unsorted:")
-    for item in list:
-        print(item)
-    sort.SelectionSort()
-    print("Sorted:")
-    for item in sort.getList():
-        print(item)
+   ListOfIntegerLists = [GenerateIntegerList(5000) for i in range(10)]
+   timingsList = []
+   for list in ListOfIntegerLists:
+       timingsList.append(getTimings(list))
+       print(getTimings(list))
+
   
     
 main()
